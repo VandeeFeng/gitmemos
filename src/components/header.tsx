cbf104bc568fe8ad7029237faaac3fa0812e99ff
vandeefeng <naoyanzifzm@hotmail.com>
2024-12-03 03:50:29 +0800

    Vandee

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -73,7 +73,7 @@ export function Header({
             )}
             {onLabelSelect && (
               <LabelFilter
-                selectedLabel={selectedLabel}
+                selectedLabel={selectedLabel || null}
                 onLabelSelect={onLabelSelect}
               />
             )}
